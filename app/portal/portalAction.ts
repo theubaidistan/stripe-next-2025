948bf9bf596fb79832adf2e9bee7c88db6237193
theubaidistan <ubaidistan395@gmail.com>
2025-12-24 23:12:59 +0500

    feat: update return_url in createPortalSession to use environment variable

--- app/portal/portalAction.ts
+++ app/portal/portalAction.ts
@@ -5,7 +5,7 @@ import { stripe } from "@/utils/stripe";
 export async function createPortalSession(customerId: string) {
   const portalSession = await stripe.billingPortal.sessions.create({
     customer: customerId,
-    return_url: `http://localhost:3000`,
+    return_url: process.env.NEXT_PUBLIC_APP_URL || `http://localhost:3000`,
   });
 
   return { id: portalSession.id, url: portalSession.url };
